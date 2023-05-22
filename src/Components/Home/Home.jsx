75404d6d30f101f2c89f2ffabf2059a747eb1b40
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-05-22 22:10:09 +0100

    Home Still On

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -14,32 +14,45 @@ const Home = () => {
 
 
                 <section id='section-main'>
+
                     <div className='main-container'>
-                        <div className='left-container'>
-                            <h1>GET <span>YOUR</span> WEBSITE</h1>
+                        <div className="background-grad">
+                            <div className='left-container'>
+                                <h1>GET <span>YOUR</span> WEBSITE</h1>
 
-                            <p>We are gonna create a well developed and designed website from your own choice and it will exactly as you desire and want .
-                                The website you want will be created with high quality ,
-                                our team which is formed with experienced programmers and designers will take of every corner.</p>
+                                <p>We are gonna create a well developed and designed website from your own choice and it will exactly as you desire and want .
+                                    The website you want will be created with high quality ,
+                                    our team which is formed with experienced programmers and designers will take of every corner.</p>
 
-                            <button>GET STARTED</button>
+                                <button>GET STARTED</button>
 
 
-                            <div className='undertext'>
-                                <BsArrowRight />
-                                <h4>CHANGE YOUR <br /> IDEA TO A BUSINESS</h4>
+                                <div className='undertext'>
+                                    <BsArrowRight />
+                                    <h4>CHANGE YOUR <br /> IDEA TO A BUSINESS</h4>
+                                </div>
+
                             </div>
 
-                        </div>
+                            <div className='right-container'>
+                                <img src={Computer} alt='computer3d' />
+                            </div>
 
-                        <div className='right-container'>
-                            <img src={Computer} alt='computer3d' />
                         </div>
                     </div>
                 </section>
 
                 <section id='section-second'>
+                    <div className="zipper-pull">
+                        <div class="scroll-downs">
+                            <div class="mousey">
+                                <div class="scroller"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='section-container'>
 
+                    </div>
                 </section>
 
             </div>
