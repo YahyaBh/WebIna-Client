e75a8170cefa075325b08f035d9f1eda6a286677
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-11-19 16:25:48 +0100

    Performance enhancments , by adding react-snap

--- src/index.js
+++ src/index.js
@@ -18,28 +18,35 @@ i18next
     supportedLngs: ['en', 'ar', 'fr'],
     fallbackLng: 'en',
     debug: false,
-    // Options for language detector
     detection: {
       order: ['path', 'cookie', 'htmlTag'],
       caches: ['cookie'],
     },
-    // react: { useSuspense: false },
     backend: {
       loadPath: '/assets/locales/{{lng}}/translation.json',
     },
   })
 
+const rootElement = document.getElementById('web-ina-digita-SAf21kDKASJ2DNAKSDML2flFKAMSD');
+const root = ReactDOM.createRoot(rootElement);
 
+const renderApp = () => {
+  root.render(
+    <Suspense fallback={<Loading />}>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </Suspense>
+  );
+};
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('web-ina-digita-SAf21kDKASJ2DNAKSDML2flFKAMSD'));
-root.render(
-  <Suspense fallback={<Loading/>}>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </Suspense>
-);
+if (rootElement.hasChildNodes()) {
+  // If the root element already has child nodes, hydrate
+  root.hydrate(<Loading />);
+  renderApp();
+} else {
+  // If the root element is empty, render
+  renderApp();
+}
