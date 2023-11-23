48198ba6ddbcda0300b2ff1891620ca4c16811ab
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-11-23 18:02:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,48 @@
-import React, { Suspense } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import i18next from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import HttpApi from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
+import './index.css';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import Loading from './Components/Loading/Loading';
 import { HelmetProvider } from 'react-helmet-async';
 
-i18next
-  .use(HttpApi)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    supportedLngs: ['en', 'ar', 'fr'],
-    fallbackLng: 'en',
-    debug: false,
-    detection: {
-      order: ['path', 'cookie', 'htmlTag'],
-      caches: ['cookie'],
-    },
-    backend: {
-      loadPath: '/assets/locales/{{lng}}/translation.json',
-    },
-  })
+const Root = () => {
+  const [loading, setLoading] = useState(true);
 
-const root = ReactDOM.createRoot(document.getElementById('web-ina-digita-SAf21kDKASJ2DNAKSDML2flFKAMSD'));
-root.render(
-  <Suspense fallback={<Loading />}>
+  useEffect(() => {
+    i18next
+      .use(HttpApi)
+      .use(LanguageDetector)
+      .use(initReactI18next)
+      .init({
+        supportedLngs: ['en', 'ar', 'fr'],
+        fallbackLng: 'en',
+        debug: false,
+        detection: {
+          order: ['path', 'cookie', 'htmlTag'],
+          caches: ['cookie'],
+        },
+        backend: {
+          loadPath: '/assets/locales/{{lng}}/translation.json',
+        },
+      })
+      .then((res) => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
     <HelmetProvider>
       <BrowserRouter>
-        <App />
+        {loading ? <Loading /> : <App />}
       </BrowserRouter>
     </HelmetProvider>
-  </Suspense>
-);
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('web-ina-digita-SAf21kDKASJ2DNAKSDML2flFKAMSD'));
+root.render(<Root />);
