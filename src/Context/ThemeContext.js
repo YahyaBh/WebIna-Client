3628fd31f3451eac33bebe799964b88901b8bebc
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-12-04 13:12:52 +0100

    Update ThemeContext.js

--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -12,15 +12,12 @@ export const ThemeProvider = ({ children }) => {
         setIsDarkMode((prevMode) => !prevMode);
     };
 
-
-
     useEffect(() => {
         const savedThemeMode = Cookies.get("__THEME_MODE");
 
         if (savedThemeMode) {
             setIsDarkMode(savedThemeMode === "true");
         } else {
-
             // Set a default theme mode if no saved value is found
             setIsDarkMode(false);
         }
@@ -79,7 +76,8 @@ export const ThemeProvider = ({ children }) => {
 
     return (
         <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
-            {loading ? <Loading /> : children}
+            {loading ? <Loading /> : ''}
+            {children}
         </ThemeContext.Provider>
     );
 };
